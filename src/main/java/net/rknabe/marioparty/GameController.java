ffb02c8fdbc32af586e1fa12345af6f12408ab9b
DerEine117 <robert.knabe@web.de>
2024-04-17 17:42:46 +0200

    added superclass GameController and extended all GameXController from it

---
+++ src/main/java/net/rknabe/marioparty/GameController.java
@@ -0,0 +1,14 @@
+package net.rknabe.marioparty;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GameController {
+    @FXML
+    protected Button backToMenu;
+
+    @FXML
+    protected void backToMenuClick() {
+        StageChanger.setScene(0);
+    }
+}
--- src/main/java/net/rknabe/marioparty/game5/Game5Controller.java
+++ src/main/java/net/rknabe/marioparty/game5/Game5Controller.java
@@ -1,15 +1,7 @@
 package net.rknabe.marioparty.game5;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import net.rknabe.marioparty.StageChanger;
+import net.rknabe.marioparty.GameController;
 
-public class Game5Controller {
-    @FXML
-    protected Button backToMenu;
+public class Game5Controller extends GameController {
 
-    @FXML
-    protected void backToMenuClick() {
-        StageChanger.setScene(0);
-    }
 }
