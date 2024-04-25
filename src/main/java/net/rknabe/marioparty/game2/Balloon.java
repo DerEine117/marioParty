07dbb69d3338797c7431f0d017e8345e18884864
Mzaex <147636689+Mzaex@users.noreply.github.com>
2024-04-25 22:29:26 +0200

    updated fxml

--- src/main/java/net/rknabe/marioparty/game2/Balloon.java
+++ src/main/java/net/rknabe/marioparty/game2/Balloon.java
@@ -1,26 +1,41 @@
 package net.rknabe.marioparty.game2;
 
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.stage.Screen;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.awt.font.ImageGraphicAttribute;
 
 public class Balloon {
-    private final double x;
+    private double x;
     private double y;
     private static final double BALLOON_RADIUS = 20.0;
+    private ImageView balloonImage;
+    private Canvas gameCanvas;
 
-     // ballon is a picture of a balloon
+
+
+    // ballon is a picture of a balloon
 
     public Balloon() {
         // this.x = random number between 0 and canvas width
+        this.x = Math.random() * gameCanvas.getWidth();
         // this.y = bottom of the canvas
-        // choose picture by random
+        this.y = gameCanvas.getHeight();
+
+        Image image = new Image("net/rknabe/marioparty/assets/Balloon.png");
+        this.balloonImage = new ImageView(image);
+        this.balloonImage.setX(this.x);
+        this.balloonImage.setY(this.y);
 
     }
-    public void move(double x, double y){
+    public void move(double y){
         // todo: move the balloon up the screen
         // aufgerufen in Game2Controller
         this.y++; //?
+        this.balloonImage.setY(this.y);
+
     }
     public void display(double x, double y){
         // displays the balloon at the given position
--- src/main/java/net/rknabe/marioparty/game2/Game2Controller.java
+++ src/main/java/net/rknabe/marioparty/game2/Game2Controller.java
@@ -2,17 +2,21 @@ package net.rknabe.marioparty.game2;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import net.rknabe.marioparty.StageChanger;
+
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 
-public class Game2Controller {
+public class Game2Controller implements Initializable {
 
     private static final int NUM_BALLOONS = 50;
     private static final double MOVE_SPEED_INCREASEMENT = 0.1;
@@ -21,27 +25,27 @@ public class Game2Controller {
     private int deploy_speed = 2000; // milliseconds
     private static final int DEPLOY_SPEED_INCREASEMENT = 500; // milliseconds
 
-
     // todo initialize the construktor and add the number of balloons to the list(NUM_BALLOONS)
-    // all the ballons initialized in the list should be invisible at the beginning
 
-
-    @FXML
-    private Canvas gameCanvas;
-    private GraphicsContext gc;
 
     @FXML
     private Button backToMenu;
-
-
-    protected void initialize(){
-        // create Person and Computer Objects
-        // todo: go through the list of balloons and make one after another visible and move up the screen
-        // iterate through list with thread that sleeps for deploy_speed milliseconds
-
-        // if balloon is clicked-> balloon.remove and Player.increaseSc the speed of the balloons
-        // if y = canvas max height -> ballon.remove()
+    @FXML
+    protected void backToMenuClick() {
+        StageChanger.setScene(0);
     }
+    @FXML
+    public Label playerScore;
+    @FXML
+    public Label computerScore;
+    @FXML
+    public Label balloonsLeft;
+
+
+    @FXML
+    public Canvas gameCanvas;
+
+    private GraphicsContext gc;
 
     @FXML
     protected void balloonClicked() {
@@ -64,17 +68,16 @@ public class Game2Controller {
         //sound effekt and pop animation
     }
 
-    @FXML
-    public Label playerScore;
 
-    @FXML
-    public Label computerScore;
 
-    @FXML
-    public Label balloonsLeft;
 
-    @FXML
-    protected void backToMenuClick() {
-        StageChanger.setScene(0);
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // create Person and Computer Objects
+        // todo: go through the list of balloons and make one after another visible and move up the screen
+        // iterate through list with thread that sleeps for deploy_speed milliseconds
+
+        // if balloon is clicked-> balloon.remove and Player.increaseSc the speed of the balloons
+        // if y = canvas max height -> ballon.remove()
     }
 }
