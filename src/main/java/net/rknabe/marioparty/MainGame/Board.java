b0663e2dc7774bd21947ae8fbf8574bb00237c2b
Mzaex <147636689+Mzaex@users.noreply.github.com>
2024-05-13 22:32:06 +0200

    dice

--- src/main/java/net/rknabe/marioparty/MainGame/Board.java
+++ src/main/java/net/rknabe/marioparty/MainGame/Board.java
@@ -8,16 +8,7 @@ public class Board {
 
 
     void initalizeBoard(GridPane gridPane){
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
-        for(int i = 0; i<8;i++){
-            for(int j = 0; j<8;j++){
-                Rectangle rectangle = new Rectangle(50,50);
-                rectangle.setFill(Color.BLUE);
-                gridPane.add(rectangle,i,j);
-            }
-
-    } }
+      }
 
     private static void setFelder() {
 
