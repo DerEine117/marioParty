3d55fb4a8996e094256b1d7b93fab3f06732fd27
Mzaex <147636689+Mzaex@users.noreply.github.com>
2024-05-14 17:22:38 +0200

    gameController

--- src/main/java/net/rknabe/marioparty/GameController.java
+++ src/main/java/net/rknabe/marioparty/GameController.java
@@ -3,15 +3,18 @@ package net.rknabe.marioparty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class GameController {
+public abstract class GameController {
     @FXML
-    protected Button backToMenu;
+    protected Button SpielanleitungButton;
+    @FXML
+    private void SpielanleitungClick() {
+        // todo: Spielanleitung anzeigen
+    }
 
+    // Methode existiert weiterhin
+    // beim Spielende soll automatisch backToMenuClick() aufgerufen werden!!!!
     @FXML
     protected void backToMenuClick() {
         StageChanger.setScene(0);
     }
-
-    // todo: Spielanleitung anzeigen
-
 }
